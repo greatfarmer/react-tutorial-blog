fa11bf4316779ef2dcdca2defd73516ccadfc6c3
Seonghun <lutis107@gmail.com>
2018-09-03 04:26:13 +0900

    Add posts limit

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -58,7 +58,10 @@ GET /api/posts
 */
 exports.list = async (ctx) => {
   try {
-    const posts = await Post.find().sort({_id: -1}).exec();
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .exec();
     ctx.body = posts;
   } catch (e) {
     ctx.throw(e, 500);
