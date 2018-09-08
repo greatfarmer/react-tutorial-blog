81b430824cc0f1a52dd00bf98cae8d3fe6ff0d4b
Seonghun <lutis107@gmail.com>
2018-09-09 02:53:28 +0900

    Create 'Header' component

--- blog-frontend/src/components/common/PageTemplate/PageTemplate.js
+++ blog-frontend/src/components/common/PageTemplate/PageTemplate.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './PageTemplate.scss';
 import classNames from 'classnames/bind';
+import Header from 'components/common/Header';
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = () => {
   return (
     <div className={cx('page-template')}>
-      PageTemplate
+      <Header/>
     </div>
   );
 };
