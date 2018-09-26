ca3a587e9ddbd41bd1159e7556b9e29e9e719684
Seonghun <lutis107@gmail.com>
2018-09-26 22:51:35 +0900

    Add setting for using session at server

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -6,10 +6,12 @@ const bodyParser = require('koa-bodyparser');
 
 const api = require('./api');
 const mongoose = require('mongoose');
+const session = require('koa-session');
 
 const {
   PORT: port = 4000, // 값이 존재하지 않는다면 4000을 기본 값으로 사용
-  MONGO_URI: mongoURI
+  MONGO_URI: mongoURI,
+  COOKIE_SIGN_KEY: signKey
 } = process.env;
 
 mongoose.Promise = global.Promise; // Node의 Promise를 사용하도록 설정
@@ -27,6 +29,15 @@ router.use('/api', api.routes()); // api 라우트 적용
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+
+// 세션/키 적용
+const sessionConfig = {
+  maxAge: 86400000, // 하루
+  // signed: true(기본으로 설정되어 있습니다.)
+};
+
+app.use(session(sessionConfig, app));
+app.keys = [signKey];
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
