08cc0dbd0bcde7699669c1b5402cd8cdaf8a1208
Seonghun <lutis107@gmail.com>
2018-10-23 00:41:53 +0900

    Load data when rendering server-side

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PageTemplate from 'components/common/PageTemplate';
 import Post from 'containers/post/Post';
 import AskRemoveModalContainer from 'containers/modal/AskRemoveModalContainer';
+import * as postActions from 'store/modules/post';
+import { bindActionCreators } from 'redux';
 
 const PostPage = ({match}) => {
   const { id } = match.params;
@@ -11,6 +13,12 @@ const PostPage = ({match}) => {
       <AskRemoveModalContainer/>
     </PageTemplate>
   );
-};
+}
+
+PostPage.preload = (dispatch, params) => {
+  const { id } = params;
+  const PostActions = bindActionCreators(postActions, dispatch);
+  return PostActions.getPost(id);
+}
 
 export default PostPage;
